05194e55bc6b4ae521a5f8e4f7f97d2a4475c7ef
Iwo Dziechciarow <iiwo@o2.pl>
2012-12-13 18:43:23 +0100

    changed generator folder path

--- lib/generators/bigos/install_generator.rb
+++ lib/generators/bigos/install_generator.rb
@@ -1,7 +1,7 @@
 module Bigos
   class InstallGenerator < Rails::Generators::Base
     include Rails::Generators::Migration
-    source_root File.expand_path('../templates', __FILE__)
+    source_root File.expand_path('../../templates', __FILE__)
 
     def self.next_migration_number(dirname)
       Time.now.strftime("%Y%m%d%H%M%S")
